4eac550a76209098dfea2730e818b40381d37386
idohtml <Jacques.kanu@gmail.com>
2023-04-05 21:58:01 +0200

    added a small hero section

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className="flex justify-around items-center py-5">
-      <h3 className="text-4xl font-bold text-sky-500">DavMoz</h3>
+      <h3 className="text-4xl font-semibold text-sky-500">DavMoz</h3>
       <nav>
         <ul className="flex gap-5 items-center text-lg font-bold uppercase text-sky-500">
           <Link className="hover:text-slate-50" href="/">
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import Davmoz from "@/public/images/David.jpg";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello, World</h1>
+      <section>
+        <div className="grid grid-cols-2 place-items-center h-screen gap-4">
+          <div>
+            <Image
+              className="rounded-xl"
+              src={Davmoz}
+              alt="Portrait of David Mozart"
+            />
+          </div>
+          <div>
+            <h1 className="text-4xl lg:text-6xl">Hi Im David 👋🏻</h1>
+            <h2 className="text-xl lg:text-3xl lg:w-2/4">
+              and Im a research engineer in Linnaeus University based in Kalmar,
+              Sweden
+            </h2>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
